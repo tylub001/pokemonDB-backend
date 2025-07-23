19cb4e23105f09b3347c6beb1f86338055d9be28
Brittany Tylutke <tylub001@gmail.com>
2025-07-23 00:08:22 +0000

    frontend chnages

--- pokemondb-server/index.js
+++ pokemondb-server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const app = express();
+const path = require('path');
 const cors = require("cors");
 const { errors } = require("celebrate");
 const mongoose = require("mongoose");
@@ -10,7 +12,6 @@ const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/loggers");
 const limiter = require("./utils/rateLimiter");
 
-const app = express();
 const { PORT = 3002 } = process.env;
 
 mongoose
@@ -20,20 +21,41 @@ mongoose
   })
   .catch(console.error);
 
-app.use(cors());
+app.use(express.static(path.join(__dirname, "../pokemonDB-frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../pokemonDB-frontend/dist/index.html"));
+});
+
+app.use(
+  cors({
+    origin: "https://pokefinal.jumpingcrab.com",
+    credentials: true,
+  })
+);
 app.use(limiter);
 app.use(express.json());
 app.use(requestLogger);
 app.use(helmet());
 
+app.get("/ping", (req, res) => {
+  console.log("Ping route reached!");
+  res.send("pong");
+});
+
+app.get('/test-backend', (req, res) => {
+  console.log('Test backend route hit!');
+  res.json({ message: 'Backend changes are live!' });
+});
+
 app.use("/", mainRouter);
 
-app.get("/ping", (req, res) => res.send("pong"));
 
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on ${PORT}`);
 });
+
