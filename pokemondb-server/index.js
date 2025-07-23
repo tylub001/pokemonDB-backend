2466a9282946a2236d34eb9f2b530ce7d57642fb
Brittany Tylutke <tylub001@gmail.com>
2025-07-23 06:06:17 -0400

    eslint

--- pokemondb-server/index.js
+++ pokemondb-server/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const path = require('path');
 const cors = require("cors");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 require("dotenv").config();
 const { errors } = require("celebrate");
 const { MONGO_URL } = require("./utils/config");
 const mainRouter = require("./routes/index");
-const helmet = require("helmet");
+
 const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/loggers");
 const limiter = require("./utils/rateLimiter");
@@ -14,18 +15,15 @@ const limiter = require("./utils/rateLimiter");
 const app = express();
 const { PORT = 3002 } = process.env;
 
-
-
 mongoose
   .connect(MONGO_URL)
   .then(() => console.log("Connected to DB"))
   .catch(console.error);
 
-app.use(express.static(path.join(__dirname, "../pokemonDB-frontend/dist")));
+app.use(express.static(path.join(__dirname, "../pokemondb")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../pokemonDB-frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../pokemondb/index.html"));
 });
-
 app.use(
   cors({
     origin: "https://pokefinal.jumpingcrab.com",
@@ -47,14 +45,6 @@ app.get('/test-backend', (req, res) => {
   res.json({ message: 'Backend changes are live!' });
 });
 
-app.use("/", mainRouter);
-
-
-
-app.get("/test-backend", (req, res) => {
-  console.log("Test backend route hit!");
-  res.json({ message: "Backend changes are live!" });
-});
 
 app.use("/", mainRouter);
 app.use(errorLogger);
@@ -62,8 +52,6 @@ app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, "0.0.0.0", () => {
-=======
-app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
 
